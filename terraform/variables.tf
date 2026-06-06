411964df5cf59c044dea34f2cfce5a2d96a5ad92
Akshay Padala <68217911+tunisterk@users.noreply.github.com>
2026-06-06 03:29:45 +0200

    add terraform gitignore and vpc module

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "AWS region"
+  default     = "eu-central-1"
+}
+
+variable "cluster_name" {
+  description = "EKS cluster name"
+  default     = "retail-store-eks"
+}
