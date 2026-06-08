4113ae7ad69e9706d278a786ee161f9668ba0dc5
Akshay Padala <68217911+tunisterk@users.noreply.github.com>
2026-06-09 00:01:39 +0200

    fix kubernetes manifests - all pods running

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,6 +6,13 @@ terraform {
     }
   }
   required_version = ">= 1.0"
+
+  backend "s3" {
+    bucket       = "retail-store-terraform-state-kiki"
+    key          = "terraform.tfstate"
+    region       = "eu-central-1"
+    use_lockfile = true
+  }
 }
 
 provider "aws" {
